f2d979b3f640a24f66ebfaa13f3b02b2409db2f5
bodin <bodinsonsawan@gmail.com>
2023-10-29 12:49:51 +0700

    edit(ex1-2): bodin's assignment

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,18 @@
 // Exercise #1: For Each Function
 
+function add5000(number){
+  return number + 5000;
+}
+
 function forEach(array, operation) {
-  // Start coding here
+  for(let i=0; i < array.length;i++){   // เอา employeesalaries ไป ทำงานใน add5000
+    newEmployeeSalaries.push(operation(array  [i]));    
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
-// Using `forEach` function here
+forEach(employeeSalaries,add5000);
 
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+console.log(newEmployeeSalaries);
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,47 @@
-//Exercise #2: At Least Five Function
+//Exercise #2: At Least Five Function  // หาจำนวนเกิน 70 ถ้าใน arrayมีเกิน 5 ให้ผลออก true
 
-function atLeastFive(array, operation) {
-  // Start coding here
+let moreThan70=[]
+function ifMoreThan70(number) {
+ 
+  if (number >= 70) {
+    moreThan70.push(number);
+  }
+  if (moreThan70.length >= 5) {
+    return true;
+  } else {
+    return false;
+    
+  }
 }
 
-const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
-const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
-const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
+function atLeastFive(array, operation) {
+  let result =[] 
+  for(let i =0; i < array.length; i++){
+result = operation(array[i])
+  } 
+  moreThan70=[]
+  return result
+}
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+
+
+
+const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67]; // 5 true
+const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100]; // 4 false
+const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67]; // 4 false
+
+
+console.log(moreThan70)
+console.log(moreThan70.length)
+console.log(ifMoreThan70())
+
+
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,ifMoreThan70);
+console.log(scoreRoom1Result);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2,ifMoreThan70);
+console.log(scoreRoom2Result);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3,ifMoreThan70);
+console.log(scoreRoom3Result);
+
+console.log(moreThan70)
